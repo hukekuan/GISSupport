e70294797df9f8eb3a76b5c54459c4c4e19b2a9e
hukekuan@163.com <husd31kews86kuan>
2017-05-10 17:43:23 +0800

    整体更新

--- src/main/java/com/gis3c/App.java
+++ src/main/java/com/gis3c/App.java
@@ -1,19 +1,49 @@
 package com.gis3c;
 
 import com.gis3c.service.HelloService;
+import org.geotools.data.FileDataStore;
+import org.geotools.data.FileDataStoreFinder;
+import org.geotools.data.simple.SimpleFeatureSource;
+import org.geotools.map.FeatureLayer;
+import org.geotools.map.Layer;
+import org.geotools.map.MapContent;
+import org.geotools.styling.SLD;
+import org.geotools.styling.Style;
+import org.geotools.swing.JMapFrame;
+import org.geotools.swing.data.JFileDataStoreChooser;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.File;
+import java.io.IOException;
 
 public class App {
     public static ApplicationContext ContextInit(){
         return new ClassPathXmlApplicationContext("classpath:spring-config.xml");
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
         ApplicationContext context =ContextInit();
+//        HelloService obj = (HelloService) context.getBean("helloService");
+//        System.out.println(obj.SayHello());
+        // display a data store file chooser dialog for shapefiles
+        File file = JFileDataStoreChooser.showOpenFile("shp", null);
+        if (file == null) {
+            return;
+        }
 
-        HelloService obj = (HelloService) context.getBean("helloService");
+        FileDataStore store = FileDataStoreFinder.getDataStore(file);
+        SimpleFeatureSource featureSource = store.getFeatureSource();
 
-        System.out.println(obj.SayHello());
+        // Create a map content and add our shapefile to it
+        MapContent map = new MapContent();
+        map.setTitle("Quickstart");
+
+        Style style = SLD.createSimpleStyle(featureSource.getSchema());
+        Layer layer = new FeatureLayer(featureSource, style);
+        map.addLayer(layer);
+
+        // Now display the map
+        JMapFrame.showMap(map);
     }
 }
