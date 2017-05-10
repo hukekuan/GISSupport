b6a8f049ec428d1037d2e90c17f095b23acf3763
hukekuan@163.com <husd31kews86kuan>
2017-05-10 18:21:49 +0800

    全局更新

--- src/main/java/com/gis3c/App.java
+++ src/main/java/com/gis3c/App.java
@@ -24,26 +24,7 @@ public class App {
 
     public static void main(String[] args) throws IOException {
         ApplicationContext context =ContextInit();
-//        HelloService obj = (HelloService) context.getBean("helloService");
-//        System.out.println(obj.SayHello());
-        // display a data store file chooser dialog for shapefiles
-        File file = JFileDataStoreChooser.showOpenFile("shp", null);
-        if (file == null) {
-            return;
-        }
-
-        FileDataStore store = FileDataStoreFinder.getDataStore(file);
-        SimpleFeatureSource featureSource = store.getFeatureSource();
-
-        // Create a map content and add our shapefile to it
-        MapContent map = new MapContent();
-        map.setTitle("Quickstart");
-
-        Style style = SLD.createSimpleStyle(featureSource.getSchema());
-        Layer layer = new FeatureLayer(featureSource, style);
-        map.addLayer(layer);
-
-        // Now display the map
-        JMapFrame.showMap(map);
+        HelloService obj = (HelloService) context.getBean("helloService");
+        System.out.println(obj.SayHello());
     }
 }
